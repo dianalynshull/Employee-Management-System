bdaead34166338a9789a69d73d3783e7740f43d4
dianalynshull <dianalynshull@gmail.com>
2020-12-27 23:05:04 -0700

    created the mysql connection in my queries file

---
+++ queries.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'password',
+  database: 'employee_db'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('successfully connected to database');
+});
